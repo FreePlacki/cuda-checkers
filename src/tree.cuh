58174794b95dbd44df8675bb9ee477906bd3a487
FreePlacki <bieleckipawel10@gmail.com>
2025-11-24 12:37:38 +0100

    Add tree structures

---
+++ src/tree.cuh
@@ -0,0 +1,152 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "math.h"
+#include "float.h"
+
+typedef struct {
+    u32 count;
+    u32 size;
+
+    Node **nodes;
+} Nodes;
+
+#define INITIAL_NODES_SZ 4
+int init_nodes(Nodes *nodes) {
+    nodes->count = 0;
+    nodes->size = INITIAL_NODES_SZ;
+
+    nodes->nodes = malloc(INITIAL_NODES_SZ * sizeof(Node *));
+    if (!nodes->nodes)
+        return 0;
+
+    return 1;
+}
+
+int nodes_add(Nodes *nodes, const Node *node) {
+    if (nodes->size == nodes->count) {
+        nodes->size <<= 1;
+        nodes->nodes = realloc(nodes->nodes, nodes->size * sizeof(Node *));
+        if (!nodes->nodes)
+            return 0;
+    }
+    nodes->nodes[nodes->count++] = node;
+    return 1;
+}
+
+void nodes_free(Nodes *nodes) {
+    if (nodes && nodes->nodes)
+        free(nodes->nodes);
+    nodes->size = nodes->count = 0;
+}
+
+typedef struct {
+    // # games played * 2
+    u32 games_played;
+    // # games won * 2 + # draws * 1
+    u32 games_won;
+
+    Node *parent;
+    Nodes children;
+    GameState gs;
+} Node;
+
+int node_init(Node *n, const Node *parent, GameState gs) {
+    n = (Node *)malloc(sizeof(Node));
+
+    n->games_played = n->games_won = 0;
+    n->parent = parent;
+    if (!nodes_init(&n->children))
+        return 1;
+    n->gs = gs;
+
+    return 0;
+}
+
+void node_free(Node *n) {
+    if (!n)
+        return;
+    for (int i = 0; i < n->children->count)
+        node_free(n->children[i]);
+    nodes_free(&n->children);
+    free(n);
+}
+
+int node_is_terminal(const Node *n) { return game_result(&n->gs) != PENDING; }
+
+void node_backprop(Node *n, GameResult result) {
+    int is_white = n->gs.current_player == WHITE;
+    switch (result) {
+    case WHITE_WON:
+        if (is_white)
+            n->games_won += 2;
+        break;
+    case BLACK_WON:
+        if (!is_white)
+            n->games_won += 2;
+        break;
+    case DRAW:
+        n->games_won += 1;
+        break;
+    case PENDING:
+        assert(0 && "unreachable");
+    }
+    n->games_played += 2;
+
+    if (n->parent)
+        node_backprop(n->parent, result);
+}
+
+#define VALUATION_C 1.414213562373095f // sqrt(2)
+float node_valuation(const Node *n) {
+    assert(n->parent);
+
+    if (n->games_played == 0)
+        return FLT_MAX;
+
+    float exploitation = (float)n->games_won / (float)n->games_played;
+    float exploration =
+        VALUATION_C *
+        sqrtf(log((float)n->parent->games_played) / (float)n->games_played);
+
+    return exploitation + exploration;
+}
+
+Node *node_select_child(const Node *root) {
+    float best_v = 0.0f;
+    Node *best_n = NULL;
+    for (int i = 0; i < root->children.count; ++i) {
+        float val = node_valuation(root->children->nodes[i]);
+        if (val > best) {
+            best_v = val;
+            best_n = root->children->nodes[i];
+        }
+    }
+
+    return best_n;
+}
+
+Node *node_select_leaf(const Node *root) {
+    assert(!root->parent);
+
+    Node *n = root;
+    // TODO
+    for (;;) {
+        Node *ch = node_select_child(n);
+        if (!ch)
+            return n;
+    }
+}
+
+Node *node_expand(Node *n, Move move) {
+    GameState gs = n->gs;
+
+    apply_move(&gs->board, move, gs->current_player == WHITE, 1);
+    next_turn(&gs, is_capture(move));
+
+    Node *child;
+    node_init(child, n, gs);
+    nodes_add(&n->children, child);
+}
+
+#endif /* TREE_H */
